72e40143207e72d611246f7c6b272263a674847d
redlolz <robinlego@hotmail.nl>
2020-05-12 13:25:56 +0200

    Uitwerking van de exercise

---
+++ caesar-cipher.php
@@ -0,0 +1,20 @@
+<?php
+function caeser($shift, $string) {
+    $chars = str_split("abcdefghijklmnopqrstuvwxyz");
+    $ciphered_string = "";
+    foreach(str_split($string) as $chr) {
+        if (in_array($chr, $chars)) {
+            $pos = array_search($chr, $chars) + $shift;
+            if ($pos > sizeof($chars)-1) {
+                $pos = $pos - sizeof($chars);
+            }
+            $ciphered_string .= $chars[$pos];
+        } else {
+            $ciphered_string .= $chr;
+        }
+    }
+    return $ciphered_string;
+}
+
+echo caeser($argv[1], $argv[2]) . PHP_EOL;
+?>
